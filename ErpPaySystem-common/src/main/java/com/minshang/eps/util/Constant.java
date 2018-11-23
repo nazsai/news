ea952cef4cb1affd7081aead93aa1b5080e08aba
Administrator <Administrator@PC-20180901HNBE.lan>
2018-11-23 13:56:34 +0800

    aaa

---
+++ ErpPaySystem-common/src/main/java/com/minshang/eps/util/Constant.java
@@ -0,0 +1,11 @@
+package com.minshang.eps.util;
+
+public class Constant {
+	
+	public static final String MODULEMAPNAME = "MODULEMAPNAME";
+	
+	public static final String INDUSTRYMAPNAME = "INDUSTRYMAPNAME";
+	
+	public static final String DISHESTYPENAME = "DISHESTYPENAME";
+	
+}
